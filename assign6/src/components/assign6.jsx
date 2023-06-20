5620f377f48810da510d34c572cb3673adc0cb21
rana <abedorana7@gmail.com>
2023-06-20 00:01:18 -0400

    fixed addRow and addColumn to work

--- assign6/src/components/assign6.jsx
+++ assign6/src/components/assign6.jsx
@@ -3,7 +3,77 @@ import TableRow from "./TableRow";
 import TableCell from "./TableCell";
 
 const Assign6 = () => {
-    //const gridRef = useRef('null')
+    const [selectedColor, setSelectedColor] = useState('');
+    const [isDragging, setIsDragging] = useState(false);
+    const gridRef = useRef(null);
+
+    const addRows = () => {
+        const newRow = document.createElement('tr');
+        const cols = gridRef.current.rows[0].cells.length;
+        for (let i = 0; i < cols; i++) {
+        const newCell = document.createElement('td');
+        // newCell.addEventListener('mousedown', changeCellColor);
+        // newCell.addEventListener('mouseup', releasedMouse);
+        newRow.appendChild(newCell);
+        }
+        gridRef.current.appendChild(newRow);
+    };
+
+    const addCols = () => {
+        const rows = gridRef.current.rows;
+        for (let i = 0; i < rows.length; i++) {
+        const newCell = document.createElement('td');
+        // newCell.addEventListener('mousedown', changeCellColor);
+        // newCell.addEventListener('mouseup', releasedMouse);
+        rows[i].appendChild(newCell);
+        }
+    };
+
+    const changeColor = (event) => {
+        setSelectedColor(event.target.value);
+    };
+
+    useEffect(() => {
+        gridRef.current = document.getElementById("grid");
+    }, []);
+
+    
+    return(
+        <div>
+
+            <h1>Cells</h1>
+
+            <button type="button" onClick = {addRows}>Add Rows</button>
+            <button type="button" onClick = {addCols}>Add Columms</button>
+            {/* <!-- <button type="button" onClick= {remRows}>Remove Rows</button>
+            <button type="button" onClick= {remCols}>Remove Columns</button> 
+            --> */}
+            <select id="color" onChange = {changeColor}>
+            <option value ="">Default</option>
+            <option value ="red">Red</option>
+            <option value ="blue">Blue</option>
+            <option value ="green">Green</option>
+            <option value ="yellow">Yellow</option>
+            </select> 
+            {/* <!--
+                <button id="colorUncolorCells" onclick="filluncolor">Color Uncolored Cells</button>
+                <button id="colorAllCells" onclick="fillall">Color All Cells</button>
+                <button id="clearAllCells" onclick="clearall">Clear All Cells</button>
+            --> */}
+            
+            <table id='grid'>
+            <tr>
+            <td className="tableCell">Table</td>
+            </tr>
+            </table>
+        </div>
+        )
+        
+    };
+export default Assign6;
+
+
+ //const gridRef = useRef('null')
     // const[selectedColor, setSelectedColor] = useState("")
     // const[table,setTable] = useState(document.querySelector('table'))
     // table.style.backgroundColor = '';
@@ -45,71 +115,3 @@ const Assign6 = () => {
 //   const remCols = () => {
 //     // ...
 //   };
-
-    
-    const [selectedColor, setSelectedColor] = useState('');
-    const [isDragging, setIsDragging] = useState(false);
-    const gridRef = useRef(null);
-
-    const addRows = () => {
-        const newRow = document.createElement('tr');
-        const cols = gridRef.current.rows[0].cells.length;
-        for (let i = 0; i < cols; i++) {
-        const newCell = document.createElement('td');
-        // newCell.addEventListener('mousedown', changeCellColor);
-        // newCell.addEventListener('mouseup', releasedMouse);
-        newRow.appendChild(newCell);
-        }
-        gridRef.current.appendChild(newRow);
-    };
-
-    const addCols = () => {
-        const rows = gridRef.current.rows;
-        for (let i = 0; i < rows.length; i++) {
-        const newCell = document.createElement('td');
-        // newCell.addEventListener('mousedown', changeCellColor);
-        // newCell.addEventListener('mouseup', releasedMouse);
-        rows[i].appendChild(newCell);
-        }
-    };
-
-
-
-
-
-
-
-
-    return(
-        <div>
-
-            <h1>Cells</h1>
-
-            <button type="button" onClick = {addRows}>Add Rows</button>
-            <button type="button" onClick = {addCols}>Add Columms</button>
-            {/* <!-- <button type="button" onClick= {remRows}>Remove Rows</button>
-            <button type="button" onClick= {remCols}>Remove Columns</button> 
-            --> */}
-            <select id="color" onChange = "changecolor">
-            <option value ="">Default</option>
-            <option value ="red">Red</option>
-            <option value ="blue">Blue</option>
-            <option value ="green">Green</option>
-            <option value ="yellow">Yellow</option>
-            </select> 
-            {/* <!--
-                <button id="colorUncolorCells" onclick="filluncolor">Color Uncolored Cells</button>
-                <button id="colorAllCells" onclick="fillall">Color All Cells</button>
-                <button id="clearAllCells" onclick="clearall">Clear All Cells</button>
-            --> */}
-            
-            <table id='grid'>
-            <tr>
-            <td className="tableCell">Table</td>
-            </tr>
-            </table>
-        </div>
-        )
-        
-    };
-export default Assign6;
